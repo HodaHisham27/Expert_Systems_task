76be6f94cc8ac4325a99f20111917f340535d5c6
Hoda Hisham <77194851+HodaHisham27@users.noreply.github.com>
2023-05-18 19:59:17 +0300

    Add files via upload

---
+++ mobile_price_classification.py
@@ -0,0 +1,78 @@
+# Load libraries
+from pandas import read_csv
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+# Load dataset
+url = "D:/workspace/mobile_prices.csv.xls"
+
+
+dataset = read_csv(url)
+
+# shape
+
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('price_range').size(),"\n")
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:20]
+y = array[:,20]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+ kfold = StratifiedKFold(n_splits=10, shuffle=True)
+ cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+ results.append(cv_results)
+ names.append(name)
+ print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+ 
+ 
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Make predictions on validation dataset
+model = SVC()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print ("\n")
+
+# Evaluate predictions
+print("Accuracy : " , accuracy_score(Y_validation, predictions),"\n")
+print ("Confusion matrix ")
+print(confusion_matrix(Y_validation, predictions),"\n")
+print(classification_report(Y_validation, predictions))
